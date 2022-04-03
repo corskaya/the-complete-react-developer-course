169b1699dd7dcc08e46b30eebe080b3bb3d1419e
corskaya <cagriorskaya@hotmail.com>
2022-04-04 00:33:24 +0300

    conditional rendering in jsx

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -4,7 +4,13 @@ console.log('App.js is running!');
 
 var app = {
   title: 'Indecision App',
-  subtitle: 'Put your life in the hands of a computer'
+  subtitle: 'Put your life in the hands of a computer',
+  options: ['One', 'Two']
+
+  // Challenge
+  //
+  // Only render the subtitle if subtitle exist - logical and operator
+  // render new p tag - if options.length > 0 'Here are your options' 'No options'
 
   // JSX - JavaScript XML
 };var template = React.createElement(
@@ -15,10 +21,15 @@ var app = {
     null,
     app.title
   ),
-  React.createElement(
+  app.subtitle && React.createElement(
     'p',
     null,
     app.subtitle
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.options.length > 0 ? 'Here are your options' : 'No options'
   ),
   React.createElement(
     'ol',
@@ -42,26 +53,32 @@ var user = {
   location: 'Istanbul'
 };
 
+function getLocation(location) {
+  if (location) {
+    return React.createElement(
+      'p',
+      null,
+      'Location: ',
+      location
+    );
+  }
+}
+
 var templateTwo = React.createElement(
   'div',
   null,
   React.createElement(
     'h1',
     null,
-    user.name
+    user.name ? user.name : 'Anonymous'
   ),
-  React.createElement(
+  user.age && user.age >= 18 && React.createElement(
     'p',
     null,
     'Age: ',
     user.age
   ),
-  React.createElement(
-    'p',
-    null,
-    'Location: ',
-    user.location
-  )
+  getLocation(user.location)
 );
 
 var appRoot = document.getElementById('app');
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -2,14 +2,21 @@ console.log('App.js is running!')
 
 var app = {
   title: 'Indecision App',
-  subtitle: 'Put your life in the hands of a computer'
+  subtitle: 'Put your life in the hands of a computer',
+  options: ['One', 'Two']
 }
+
+// Challenge
+//
+// Only render the subtitle if subtitle exist - logical and operator
+// render new p tag - if options.length > 0 'Here are your options' 'No options'
 
 // JSX - JavaScript XML
 var template = (
   <div>
     <h1>{app.title}</h1>
-    <p>{app.subtitle}</p>
+    {app.subtitle && <p>{app.subtitle}</p>}
+    <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
     <ol>
       <li>Item one</li>
       <li>Item two</li>
@@ -23,11 +30,17 @@ var user = {
   location: 'Istanbul'
 }
 
+function getLocation(location) {
+  if (location) {
+    return <p>Location: {location}</p>
+  }
+}
+
 var templateTwo = (
   <div>
-    <h1>{user.name}</h1>
-    <p>Age: {user.age}</p>
-    <p>Location: {user.location}</p>
+    <h1>{user.name ? user.name : 'Anonymous'}</h1>
+    {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+    {getLocation(user.location)}
   </div>
 )
 
