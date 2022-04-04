32da0a4980b61dc506c7d72479d356ef049fff15
corskaya <cagriorskaya@hotmail.com>
2022-04-04 10:26:49 +0300

    add form element

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -3,52 +3,48 @@ console.log('App.js is running!')
 const app = {
   title: 'Indecision App',
   subtitle: 'Put your life in the hands of a computer',
-  options: ['One', 'Two']
+  options: []
 }
 
-// JSX - JavaScript XML
-const template = (
-  <div>
-    <h1>{app.title}</h1>
-    {app.subtitle && <p>{app.subtitle}</p>}
-    <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-    <ol>
-      <li>Item one</li>
-      <li>Item two</li>
-    </ol>
-  </div>
-)
+const onFormSubmit = (e) => {
+  e.preventDefault()
 
-let count = 0
+  const option = e.target.elements.option.value
 
-const addOne = () => {
-  count++
-  renderCounterApp()
+  if (option) {
+    app.options.push(option)
+    e.target.elements.option.value = ''
+    render()
+  }
 }
 
-const minusOne = () => {
-  count--
-  renderCounterApp()
-}
-
-const reset = () => {
-  count = 0
-  renderCounterApp()
+const onRemoveAll = () => {
+  app.options = []
+  render()
 }
 
 const appRoot = document.getElementById('app')
 
-const renderCounterApp = () => {
-  const templateTwo = (
+const render = () => {
+  const template = (
     <div>
-      <h1>Count: {count}</h1>
-      <button onClick={addOne}>+1</button>
-      <button onClick={minusOne}>-1</button>
-      <button onClick={reset}>Reset</button>
+      <h1>{app.title}</h1>
+      {app.subtitle && <p>{app.subtitle}</p>}
+      <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+      <p>{app.options.length}</p>
+      <ol>
+        <li>Item one</li>
+        <li>Item two</li>
+      </ol>
+      <button onClick={onRemoveAll}>Remove All</button>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option" />
+        <button>Add Option</button>
+      </form>
     </div>
   )
 
-  ReactDOM.render(templateTwo, appRoot)
+  ReactDOM.render(template, appRoot)
 }
 
-renderCounterApp()
+render()
