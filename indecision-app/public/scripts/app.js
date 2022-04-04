5701ac73f3e6e44424c6af71ebe57b3b26975816
corskaya <cagriorskaya@hotmail.com>
2022-04-04 09:36:08 +0300

    rerendering

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -42,51 +42,53 @@ var app = {
   )
 );
 
-// Challenge
-//
-// Make button "-1" -> prints minusOne runs when clicked
-// Make reset button -> prints reset runs when clicked
-
 var count = 0;
 
 var addOne = function addOne() {
-  console.log('addOne runs');
+  count++;
+  renderCounterApp();
 };
 
 var minusOne = function minusOne() {
-  console.log('minusOne runs');
+  count--;
+  renderCounterApp();
 };
 
 var reset = function reset() {
-  console.log('reset runs');
+  count = 0;
+  renderCounterApp();
 };
-
-var templateTwo = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    'Count: ',
-    count
-  ),
-  React.createElement(
-    'button',
-    { onClick: addOne },
-    '+1'
-  ),
-  React.createElement(
-    'button',
-    { onClick: minusOne },
-    '-1'
-  ),
-  React.createElement(
-    'button',
-    { onClick: reset },
-    'Reset'
-  )
-);
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(templateTwo, appRoot);
+var renderCounterApp = function renderCounterApp() {
+  var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      'Count: ',
+      count
+    ),
+    React.createElement(
+      'button',
+      { onClick: addOne },
+      '+1'
+    ),
+    React.createElement(
+      'button',
+      { onClick: minusOne },
+      '-1'
+    ),
+    React.createElement(
+      'button',
+      { onClick: reset },
+      'Reset'
+    )
+  );
+
+  ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounterApp();
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -19,34 +19,36 @@ const template = (
   </div>
 )
 
-// Challenge
-//
-// Make button "-1" -> prints minusOne runs when clicked
-// Make reset button -> prints reset runs when clicked
-
 let count = 0
 
 const addOne = () => {
-  console.log('addOne runs')
+  count++
+  renderCounterApp()
 }
 
 const minusOne = () => {
-  console.log('minusOne runs')
+  count--
+  renderCounterApp()
 }
 
 const reset = () => {
-  console.log('reset runs')
+  count = 0
+  renderCounterApp()
 }
-
-const templateTwo = (
-  <div>
-    <h1>Count: {count}</h1>
-    <button onClick={addOne}>+1</button>
-    <button onClick={minusOne}>-1</button>
-    <button onClick={reset}>Reset</button>
-  </div>
-)
 
 const appRoot = document.getElementById('app')
 
-ReactDOM.render(templateTwo, appRoot)
+const renderCounterApp = () => {
+  const templateTwo = (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={addOne}>+1</button>
+      <button onClick={minusOne}>-1</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  )
+
+  ReactDOM.render(templateTwo, appRoot)
+}
+
+renderCounterApp()
