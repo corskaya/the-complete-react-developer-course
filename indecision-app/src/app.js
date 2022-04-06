f57d09be9b4b5140eb70b7f45c2a8d9fc6be8952
corskaya <cagriorskaya@hotmail.com>
2022-04-06 10:20:49 +0300

    split up components to separate files

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,15 +1,5 @@
-// import './utils'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import IndecisionApp from './components/IndecisionApp'
 
-// subtract is default here, square and add are named
-import subtract, { square, add } from './utils'
-
-console.log('app.js is running')
-console.log(square(2))
-console.log(add(100, 23))
-console.log(subtract(100, 80))
-
-import isSenior, { isAdult, canDrink } from './person'
-
-console.log(isAdult(15))
-console.log(canDrink(36))
-console.log(isSenior(63))
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
