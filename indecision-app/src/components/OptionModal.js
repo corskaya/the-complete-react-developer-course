89744c69b836a7d871b557a6cb7edb46431f2563
corskaya <cagriorskaya@hotmail.com>
2022-04-07 02:26:12 +0300

    refactor to arrow function shorthand syntax

--- indecision-app/src/components/OptionModal.js
+++ indecision-app/src/components/OptionModal.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Modal from 'react-modal'
 
-const OptionModal = (props) => (
+const OptionModal = props => (
   <Modal
     isOpen={!!props.selectedOption}
     onRequestClose={props.handleClearSelectedOption}
