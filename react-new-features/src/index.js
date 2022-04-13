2ec440e75efe552bd8f31342dc347c33c80ebb82
corskaya <cagriorskaya@hotmail.com>
2022-04-13 11:47:56 +0300

    notes example with usestate

--- react-new-features/src/index.js
+++ react-new-features/src/index.js
@@ -2,29 +2,52 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 
-const App = props => {
-  const [count, setCount] = useState(props.count)
-  const [text, setText] = useState('')
+const NoteApp = () => {
+  const [notes, setNotes] = useState([])
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+
+  const addNote = e => {
+    e.preventDefault()
+    setNotes([
+      ...notes,
+      {
+        title,
+        body
+      }
+    ])
+    setTitle('')
+    setBody('')
+  }
+
+  const removeNote = title => {
+    setNotes(notes.filter(note => note.title !== title))
+  }
 
   return (
     <div>
-      <p>The current {text || 'count'} is {count}</p>
-      <button onClick={() => setCount(count + 1)}>+1</button>
-      <button onClick={() => setCount(count - 1)}>-1</button>
-      <button onClick={() => setCount(props.count)}>reset</button>
-      <input value={text} onChange={e => setText(e.target.value)} />
+      <h1>Notes</h1>
+      {notes.map((note, index) => (
+        <div key={index}>
+          <h3>{note.title}</h3>
+          <p>{note.body}</p>
+          <button onClick={() => removeNote(note.title)}>x</button>
+        </div>
+      ))}
+      <p>Add note</p>
+      <form onSubmit={addNote}>
+        <input value={title} onChange={e => setTitle(e.target.value)} />
+        <textarea value={body} onChange={e => setBody(e.target.value)} ></textarea>
+        <button>add note</button>
+      </form>
     </div>
   )
-}
-
-App.defaultProps = {
-  count: 0
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <NoteApp />
   </React.StrictMode>
 );
 
