d0970a548a06bf7edc838e39919850764ccec517
corskaya <cagriorskaya@hotmail.com>
2022-06-01 10:33:04 +0300

    snapshot test with dynamic components

--- expensify-app/src/components/ExpenseListItem.js
+++ expensify-app/src/components/ExpenseListItem.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 
-const ExpenseListItem = ({ description, amount, createdAt, id }) => (
+export const ExpenseListItem = ({ description, amount, createdAt, id }) => (
   <div>
     <Link to={`/edit/${id}`}>
       <h3 >{description}</h3>
